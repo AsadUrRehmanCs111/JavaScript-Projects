612f0a331402186d39064b4f286c0d014d31b52a
AsadUrRehmanCs111 <55739556+AsadUrRehmanCs111@users.noreply.github.com>
2021-01-07 20:39:27 +0500

    🔥 Remove Unnecessory Code

--- 02.Counter App/app.js
+++ 02.Counter App/app.js
@@ -5,7 +5,7 @@ const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach(btn => {
-    btn.addEventListener("click", function (e) {
+    btn.addEventListener("click", e => {
         const styles = e.currentTarget.classList;
         console.log(styles)
 
