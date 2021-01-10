fbcf3a5b5488611e84731406aeae56a6d43979bc
AsadUrRehmanCs111 <55739556+AsadUrRehmanCs111@users.noreply.github.com>
2021-01-10 21:18:51 +0500

    ⚡️ Modified

--- 03. Color Flipper App/main.js
+++ 03. Color Flipper App/main.js
@@ -1,15 +1,24 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+//const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
-    const randomNumber = getRandomNumber();
+    const randomColor = getRandomColor();
     // console.log(randomNumber);
 
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
+    document.body.style.backgroundColor = randomColor;
+    color.textContent = randomColor;
 });
 
-function getRandomNumber() {
-    return Math.floor(Math.random() * colors.length);
+// function getRandomNumber() {
+//     return Math.floor(Math.random() * colors.length);
+// }
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
