0f3e0cf6971c7731f4cc330a64585aa9f8ded9e2
AsadUrRehmanCs111 <55739556+AsadUrRehmanCs111@users.noreply.github.com>
2021-01-13 00:07:06 +0500

    💄 Styles Added to the App

---
+++ 04.Todo-list App/app.js
@@ -0,0 +1,9 @@
+// Select Elements
+var clearBtn = document.getElementsByClassName('clearStorage');
+console.log(clearBtn)
+var dateItem = document.getElementsByClassName('date');
+console.log(dateItem);
+var itemsList = document.getElementsByClassName('item');
+console.log(itemsList);
+
+// Displaying To Day's Date:
